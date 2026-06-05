76f7fd1fc931f2c9bd56a73f7539d1773fc18943
Hafsa Imran <hafsaemran1712@gmail.com>
2026-06-05 12:21:46 +0500

    Todo-App

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-let addTask = document.getElementById("addTask");
+let add = document.getElementById("add");
 let task = document.getElementById("task");
 let list = document.getElementById("task-list");
+let currentTodo = null;
+add.addEventListener("click", () => {
+  if (add.innerHTML === '<i class="fa-solid fa-plus"></i> Add') {
 
-addTask.addEventListener("click", () => {
-    list.innerHTML += `<div class="main"><li>${task.value}</li><div>
-  <button onclick="edit(this)">edit</button>
-  <button onclick="deleted(this)"><i class="fa-solid fa-trash"></i></button></div></div>`;
-  task.value = " ";
+    list.innerHTML += `
+      <div class="main">
+        <li>${task.value}</li>
+        <div>
+          <button class="editDeleteBtn" onclick="edit(this)">
+            <i class="fa-solid fa-pen"></i>
+          </button>
+          <button class="editDeleteBtn" onclick="deleted(this)">
+            <i class="fa-solid fa-trash"></i>
+          </button>
+        </div>
+      </div>`;
+
+    task.value = "";
+
+  } else {
+
+    currentTodo.innerText = task.value;
+    task.value = "";
+
+    add.innerHTML = '<i class="fa-solid fa-plus"></i> Add';
+  }
 });
 
 function edit(element) {
-    element.addEventListener("click", () => {
-        task.value=element.parentNode.parentNode.firstChild.innerText;
-        addTask.innerHTML = "Edit"
-    })
+  currentTodo = element.parentNode.parentNode.firstElementChild;
+  task.value = currentTodo.innerText;
+
+  add.innerHTML = '<i class="fa-solid fa-check"></i>';
 }
 
-function deleted(element){
-    element.addEventListener("click", () => {
-        element.parentNode.parentNode.innerHTML= " ";
-    })
+function deleted(element) {
+  element.parentNode.parentNode.remove();
+  task.value = "";
 }
